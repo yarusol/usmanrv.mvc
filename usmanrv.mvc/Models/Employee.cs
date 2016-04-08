c7616b0e8c648bcfb60f1c9bf59e4111998edde8
Yarovyy Ruslan <yarusol@gmail.com>
2016-04-08 13:35:53 +0300

    Added the model -- file Employee.cs

---
+++ usmanrv.mvc/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace usmanrv.mvc.Models
+{
+	public class Employee
+	{
+		[Required( AllowEmptyStrings = false, ErrorMessage = "Enter the name of employee, please." )]
+		public string Name { get; set; }
+
+		[Required( AllowEmptyStrings = false, ErrorMessage = "Enter the surname of employee, please." )]
+		public string Surname { get; set; }
+
+		[Range( 0, 100 )]
+		public byte Age { get; set; }
+
+		public string Profession { get; set; }
+	}
+}
