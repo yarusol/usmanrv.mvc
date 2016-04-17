7dab9cd2eabe1c007cbac399cd17b605af40c88e
Yarovyy Ruslan <yarusol@gmail.com>
2016-04-17 08:40:43 +0300

    Adds little refactoring in add-employee.js -- moves function validateForm( form ) into function sendByJS()

--- usmanrv.mvc/Scripts/Pages/Home/add-employee.js
+++ usmanrv.mvc/Scripts/Pages/Home/add-employee.js
@@ -3,6 +3,7 @@
 	function ( event ) {
 
 		setClickhandler( "sendJS", sendByJS );
+		return;
 
 
 		function setClickhandler( buttonId, clickHandler ) {
@@ -25,7 +26,7 @@
 
 			targetElement.addEventListener( "click", sendByJS );
 
-		}
+		}// function setClickhandler( buttonId, clickHandler )
 
 		function sendByJS() {
 			var table = document.getElementById( "registered" );
@@ -43,161 +44,162 @@
 				cell.innerHTML = i;
 			}
 
-			//validateForm(newRow);
 			var form = document.getElementById( "employeeForm" );
 			validateForm( form );
 
-		}
-
-		function validateForm( form ) {
-
-			var validationErrorsDiv = null;
-			var validableInputs = null;
-			if ( !initializeVariables() ) {
-				return;
-			}
-
-			emptyValidationErrorsDiv();
-			validableInputs.forEach( validateInput );
-
 			return;
 
+			function validateForm( form ) {
 
-			function initializeVariables() {
-
-				if ( form.elements === undefined ) {
-					console.error( "validateForm: the passed parameter is not an HTMLFormElement." );
-					return false;
+				var validationErrorsDiv = null;
+				var validableInputs = null;
+				if ( !initializeVariables() ) {
+					return;
 				}
 
-				validationErrorsDiv = getvalidationErrorsDiv();
-				if ( validationErrorsDiv === null ) {
-					console.info( "validateForm : there is no <div> for validation errors in the passed form" );
-					return false;
-				}
+				emptyValidationErrorsDiv();
+				validableInputs.forEach( validateInput );
 
-				validableInputs = getValidableInputs();
+				return;
 
-				return true;
 
-				function getvalidationErrorsDiv() {
-					var allDivs = form.getElementsByTagName( "div" );
-					for ( var i = 0 ; i < allDivs.length ; i++ ) {
-						var div = allDivs[i];
-						if ( div.hasAttribute( "data-valmsg-summary" ) ) {
-							return div;
-						}
+				function initializeVariables() {
+
+					if ( form.elements === undefined ) {
+						console.error( "validateForm: the passed parameter is not an HTMLFormElement." );
+						return false;
 					}
-					return null;
-				}
-				function getValidableInputs() {
-					var needInputs = [];
-					Array.prototype.forEach.call(
-						form.elements,
-						( function ( element, index, array ) {
+
+					validationErrorsDiv = getvalidationErrorsDiv();
+					if ( validationErrorsDiv === null ) {
+						console.info( "validateForm : there is no <div> for validation errors in the passed form" );
+						return false;
+					}
+
+					validableInputs = getValidableInputs();
+
+					return true;
+
+					function getvalidationErrorsDiv() {
+						var allDivs = form.getElementsByTagName( "div" );
+						for ( var i = 0 ; i < allDivs.length ; i++ ) {
+							var div = allDivs[i];
+							if ( div.hasAttribute( "data-valmsg-summary" ) ) {
+								return div;
+							}
+						}
+						return null;
+					}
+					function getValidableInputs() {
+						var needInputs = [];
+						Array.prototype.forEach.call(
+							form.elements,
+							( function ( element, index, array ) {
+								if ( element.tagName !== "INPUT" || element.type !== "text" ) {
+									return;
+								}
+								if ( element.hasAttribute( "data-val" ) && element.getAttribute( "data-val" ) === "true" ) {
+									needInputs.push( element );
+								}
+							} )
+						);
+						return needInputs;
+
+						function processFormElement( element, index, array ) {
 							if ( element.tagName !== "INPUT" || element.type !== "text" ) {
 								return;
 							}
 							if ( element.hasAttribute( "data-val" ) && element.getAttribute( "data-val" ) === "true" ) {
 								needInputs.push( element );
 							}
-						} )
-					);
-					return needInputs;
-
-					function processFormElement( element, index, array ) {
-						if ( element.tagName !== "INPUT" || element.type !== "text" ) {
-							return;
-						}
-						if ( element.hasAttribute( "data-val" ) && element.getAttribute( "data-val" ) === "true" ) {
-							needInputs.push( element );
 						}
 					}
 				}
-			}
-			function emptyValidationErrorsDiv() {
-				validationErrorsDiv.classList.remove( "validation-summary-errors" );
-				validationErrorsDiv.classList.add( "validation-summary-valid" );
-				var li = validationErrorsDiv.getElementsByTagName( "li" )[0];
-				if ( li === undefined ) {
-					return;
-				}
-				var ul = li.parentElement;
-				while ( li !== undefined ) {
-					ul.removeChild( li );
-					li = validationErrorsDiv.getElementsByTagName( "li" )[0];
-				}
-			}
-			function validateInput( input, index, array ) {
-				input.classList.remove( "input-validation-error" );
-				var attributes = input.getAttributeNames();
-				attributes.forEach( validateByAttribute );
-
-				return;
-
-				function validateByAttribute( attributeName ) {
-					switch ( attributeName ) {
-						case "data-val-required":
-							checkIsFilled();
-							break;
-						case "data-val-number":
-							checkIsNumber();
-							break;
-						case "data-val-range":
-							checkIsInRange();
-							break;
-						case "data-val-regex":
-							checkWithRegex();
-							break;
+				function emptyValidationErrorsDiv() {
+					validationErrorsDiv.classList.remove( "validation-summary-errors" );
+					validationErrorsDiv.classList.add( "validation-summary-valid" );
+					var li = validationErrorsDiv.getElementsByTagName( "li" )[0];
+					if ( li === undefined ) {
+						return;
 					}
+					var ul = li.parentElement;
+					while ( li !== undefined ) {
+						ul.removeChild( li );
+						li = validationErrorsDiv.getElementsByTagName( "li" )[0];
+					}
+				}
+				function validateInput( input, index, array ) {
+					input.classList.remove( "input-validation-error" );
+					var attributes = input.getAttributeNames();
+					attributes.forEach( validateByAttribute );
+
 					return;
 
+					function validateByAttribute( attributeName ) {
+						switch ( attributeName ) {
+							case "data-val-required":
+								checkIsFilled();
+								break;
+							case "data-val-number":
+								checkIsNumber();
+								break;
+							case "data-val-range":
+								checkIsInRange();
+								break;
+							case "data-val-regex":
+								checkWithRegex();
+								break;
+						}
+						return;
 
-					function checkIsFilled() {
-						if ( input.value.length == 0 ) {
-							var message = input.getAttribute( attributeName );
-							addError(input, message );
-						}
-					}// function checkIsFilled()
-					function checkIsNumber() {
-						if( input.value.length != 0 && !Number.isInteger( +input.value ) ){
-							var message = input.getAttribute( attributeName );
-							addError(input, message );
-						}
-					}// function checkIsNumber()
-					function checkIsInRange() {
-						if ( input.value.length != 0 && Number.isInteger( +input.value ) ) {
-							var value = +input.value;
-							var min = +input.getAttribute( "data-val-range-min" );
-							var max = +input.getAttribute( "data-val-range-max" );
-							if ( value < min || value > max ) {
-								var message = input.getAttribute( attributeName );
-								addError(input, message );
-							}
-						}
-					}// function checkIsInRange()
-					function checkWithRegex() {
-						if ( input.value.length != 0 ) {
-							var regexp = new RegExp( input.getAttribute( "data-val-regex-pattern" ) );
-							if ( !regexp.test( input.value ) ) {
+
+						function checkIsFilled() {
+							if ( input.value.length == 0 ) {
 								var message = input.getAttribute( attributeName );
 								addError( input, message );
 							}
-						}
-					}// function checkIsGreatherOrEqual() 
+						}// function checkIsFilled()
+						function checkIsNumber() {
+							if ( input.value.length != 0 && !Number.isInteger( +input.value ) ) {
+								var message = input.getAttribute( attributeName );
+								addError( input, message );
+							}
+						}// function checkIsNumber()
+						function checkIsInRange() {
+							if ( input.value.length != 0 && Number.isInteger( +input.value ) ) {
+								var value = +input.value;
+								var min = +input.getAttribute( "data-val-range-min" );
+								var max = +input.getAttribute( "data-val-range-max" );
+								if ( value < min || value > max ) {
+									var message = input.getAttribute( attributeName );
+									addError( input, message );
+								}
+							}
+						}// function checkIsInRange()
+						function checkWithRegex() {
+							if ( input.value.length != 0 ) {
+								var regexp = new RegExp( input.getAttribute( "data-val-regex-pattern" ) );
+								if ( !regexp.test( input.value ) ) {
+									var message = input.getAttribute( attributeName );
+									addError( input, message );
+								}
+							}
+						}// function checkIsGreatherOrEqual() 
 
-					function addError( input,  message ) {
-						input.classList.add( "input-validation-error" );
-						var errorsUL = validationErrorsDiv.firstElementChild;
-						var errorLI = document.createElement( "li" );
-						errorLI.innerHTML = message;
-						errorsUL.appendChild( errorLI );
-						validationErrorsDiv.classList.add( "validation-summary-errors" );
-						validationErrorsDiv.classList.remove( "validation-summary-valid" );
-					}
-				}// function validateByAttribute( attributeName )
+						function addError( input, message ) {
+							input.classList.add( "input-validation-error" );
+							var errorsUL = validationErrorsDiv.firstElementChild;
+							var errorLI = document.createElement( "li" );
+							errorLI.innerHTML = message;
+							errorsUL.appendChild( errorLI );
+							validationErrorsDiv.classList.add( "validation-summary-errors" );
+							validationErrorsDiv.classList.remove( "validation-summary-valid" );
+						}
+					}// function validateByAttribute( attributeName )
+				}
 			}
-		}
+		}// function sendByJS()
+
 
 
 	}
